64e5672de3491240af299217a65749236e3eb0de
023-dev <0_2_3@naver.com>
2025-02-22 00:23:24 +0900

    add(post): Add post > algorithms > boj 1351

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -11,6 +11,14 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LDJQ09Q3V9"></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date());
+        gtag('config', 'G-LDJQ09Q3V9');`}
+      </script>
     </Helmet>
   )
 }
