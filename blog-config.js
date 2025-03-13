c9c3cde12ef6c72c5b575bbb8790cd4453997aba
023-dev <0_2_3@naver.com>
2025-03-14 03:44:31 +0900

    add(post): Add post > java > effective java > item 88

--- blog-config.js
+++ blog-config.js
@@ -16,14 +16,14 @@ module.exports = {
   // See https://giscus.app/
   giscus: {
     repo: "023-dev/023-dev.github.io",
-    repoId: "R_kgDONKt8Yg=",
+    repoId: "R_kgDONKt8Yg",
     category: "Comments",
-    categoryId: "DIC_kwDONKt8Ys4Cll2b",
+    categoryId: "DIC_kwDONKt8Ys4Cn_zt",
     mapping: "pathname",
     strict: "0",
     reactionsEnabled: "1",
-    inputPosition: "bottom",
-    lang: "ko",
     emitMetadata: "0",
+    inputPosition: "top",
+    lang: "ko",
   },
 }
