4d3d062f2715d01baad8a5bd8edf366365d76701
023-dev <0_2_3@naver.com>
2025-12-24 02:56:31 +0900

    fix: localStorage 사용 시 getItem 함수 존재 여부 확인 추가

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -22,7 +22,7 @@ const Layout = ({ children }) => {
   }
 
   let localTheme = null
-  if (typeof localStorage !== "undefined") {
+  if (typeof localStorage !== "undefined" && typeof localStorage.getItem === "function") {
     localTheme = localStorage.getItem("theme")
   }
 
